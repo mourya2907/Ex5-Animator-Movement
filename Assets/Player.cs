f42dd42dfd4bd9385b6acbfb4fe58b88567b13a6
mourya2907 <mouryamoo2907@gmail.com>
2025-05-31 15:26:50 +0530

    Update Player.cs

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
-    
-    void start()
+    public Animator animator;
+    public float InputX;
+    public float InputY;
+    // Start is called before the first frame update
+    void Start()
     {
-        animator = this.gameObject.GetComponent<Animator>();
+        animator = this.gameObject.GetComponent < Animator>();
     }
 
+    // Update is called once per frame
     void Update()
     {
-        // type the code here
+        InputY = Input.GetAxis("Vertical");
+        InputX = Input.GetAxis("Horizontal");
+        animator.SetFloat("InputY", InputY);
+        animator.SetFloat("InputX", InputX);
     }
 }
